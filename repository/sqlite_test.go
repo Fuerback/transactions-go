01bdb9a7b5498090456cacad5a96098c6a6badf4
Fuerback <felipefuerback@hotmail.com>
2021-04-03 17:21:07 -0300

    testes do repositorio

---
+++ repository/sqlite_test.go
@@ -0,0 +1,68 @@
+package repository_test
+
+import (
+	"testing"
+
+	"github.com/Fuerback/transactions-go/dto"
+	"github.com/Fuerback/transactions-go/repository"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	repo           repository.Repository
+	validAccountID int64
+)
+
+func init() {
+	repo = repository.NewSqlite("../db/transaction_test.db?_foreign_keys=on")
+	cleanDB()
+}
+
+func TestCreateValidAccount(t *testing.T) {
+	repo = repository.NewSqlite("../db/transaction_test.db?_foreign_keys=on")
+
+	account := new(dto.CreateAccount)
+	account.DocumentNumber = "5457647"
+
+	ID, err := repo.CreateAccount(account)
+
+	assert.NoError(t, err)
+	assert.Greater(t, ID, int64(1))
+	validAccountID = ID
+}
+
+func TestFindValidAccount(t *testing.T) {
+	repo = repository.NewSqlite("../db/transaction_test.db?_foreign_keys=on")
+
+	account, err := repo.FindAccount(validAccountID)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, account)
+}
+
+func TestCreateValidTransaction(t *testing.T) {
+	repo = repository.NewSqlite("../db/transaction_test.db?_foreign_keys=on")
+
+	transaction := new(dto.CreateTransaction)
+	transaction.AccountID = validAccountID
+	transaction.Amount = 123.78
+	transaction.OperationTypeID = 1
+
+	ID, err := repo.CreateTransaction(transaction)
+
+	assert.NoError(t, err)
+	assert.Greater(t, ID, int64(1))
+}
+
+func TestClearDB(t *testing.T) {
+	repo = repository.NewSqlite("../db/transaction_test.db?_foreign_keys=on")
+
+	err := repo.ClearUp()
+
+	assert.NoError(t, err)
+}
+
+func cleanDB() {
+	repo.ClearUp()
+}
